7ebb2a3b68857053ae89b48f370cf3a4460587f5
Maxime <Maxime@betsy.local>
2016-05-23 20:04:09 +0200

    Update readme and added geometric progression

--- readme.md
+++ readme.md
@@ -1,3 +1,26 @@
 ##PCR efficiency
 Forked from [PCR efficiency Calculator](http://srvgen.upct.es/efficiency.html) by [Izaskun mallona](izaskun.mallona@gmail.com)
+
 Run testing.py to get a prediction
+
+**Synopsys**
+
+`python testing.py Target_sequence Forward_primer_sequence Reverse_primer_sequence`
+
+**Output**
+
+Number of amplicon sequenced from 1 template at each cycle
+
+**Sidenote**
+
+Geometric progression to find initial number of templates
+
+```
+u(n) = q^n * u(0)
+u(0) = u(n) / q^n
+
+n : number of PCR cycles
+u(n) : number of amplicon at the end of the PCR
+u(0) : number of templates at beginning of PCR
+q : efficiency
+```
--- testing.py
+++ testing.py
@@ -26,57 +26,82 @@ from subprocess import call
 from amplicon import *
 from predict import *
 import operator
+import sys
+
+def nb_init_templates (nb_cycle, nb_amplicon_final, efficiency) :
+	'''
+	nb_cycle : number of cycles of the PCR
+	nb_amplicon_final : number of amplicon at the end of PCR
+	efficiency : calculated with PCR efficiency
+	'''
+	nb_init = (nb_amplicon_final) / (efficiency**nb_cycle)
+	return nb_init
+
+def actual_prog(seq, forward, reverse) :
+	query = Amplicon()
+	query.setLabel(name)
+	query.setSequence(seq)
+
+	#check for non [ACTG] chars
+	query.cleanSequence()
+
+	forward = forward.upper()
+	reverse = reverse.upper()
+
+	query.setSequence(query.sequence.upper())
+	query.setPrimerPair(forward,reverse)
+
+	#check for non [ACTG] chars
+	query.cleanSequence()
+
+	#foo = html()
+	#foo.cssUp()
+
+	checking = query.checkHybridization(query.getSequence(),query.forward.sequence,query.reverse.sequence)
+	query.setSequence(checking[1])
 
 
-name = "test"
-seq = "GTAACAAGGTTTCCGTAGGTGAACATGCGGAAGGATCATTGTCGAACCCTGCAAAGCAGAACGACCCGTGAACACGTAAAAACAACCGAGCGTCGAGTGGATTTTTTGATCCACTCGATGCTCTGTCGATGCCCATTTACTTGTGTTCTTTTGGACTCGGTGGATGTGTCATTGACGCAATAACAACCCCCGGCACAATGTGTGCCAAGGAAAACTAAACTTGAGAATGCTTGTTTCATGTTGCCCCGTTCGCGGTGTGCTCTGGATTGGCTTCTTTATAATTACAAACGACTCTCGGCAACGGATATCTCGGCTCACGCATCGATGAAGAACGTAGCAAAATGCGATACTTGGTGTGAATTGCAGAATCCCGTGAACCATCGAGTTTTTGAACGCAAGTTGCGCCCGAAGCCTTTTGGCCGAGGGCACGTCTGCCTGGGCGTCACGCATCGCGTCGCCCCCAACAAATCTTTGTTGGGAGCGGATATTGGTCTCCCGTGCTCATGGCGTGGTTGGCCAAAATAGGAGTCCCTTCGATGGACGCACGAACTAGTGGTGGTCGTAAAAACCCTCGTTCTTTGTTCTGCGTTAGTCGTAAGAAAATACTCTTCAAACACCCCAATGTGTTGTCTTAGGACGACGCTTCGACCGCGACCCCAGGTCAGGCGGGACTACCCGCTGAGTTTAAGCATATCAATAA"
-reverse = "GGAAGTAAAAGTCGTAACAAGG"
-forward = "TCCTCCGCTTATTGATATGC"
+	ampliconList = []
+	ampliconList.append(query)
+
+	#query.checkHybridization()
+	myPredict = predict()
+	myPredict.writeHandleForR(ampliconList,1)
+	myListOfEfficiencies = myPredict.predictGam()
+
+	ampliconList[0].setEfficiency(myListOfEfficiencies[0])
+	#print name
+	effect = round(float("".join(myListOfEfficiencies)),2)
+	print str(effect)+" amplicon are synthesized from 1 template at each cycle of the PCR for the this PCR reaction"
 
 
-query = Amplicon()
-query.setLabel(name)
-query.setSequence(seq)
+	call(["rm", "gamResult.data"])
+	call(["rm", "primerDataForR.dat"])
 
-#check for non [ACTG] chars
-query.cleanSequence()
-
-forward = forward.upper()
-reverse = reverse.upper()
-
-query.setSequence(query.sequence.upper())
-query.setPrimerPair(forward,reverse)
-
-#check for non [ACTG] chars
-query.cleanSequence()
-
-#foo = html()
-#foo.cssUp()
-
-checking = query.checkHybridization(query.getSequence(),query.forward.sequence,query.reverse.sequence)
-query.setSequence(checking[1])
-
-	
-ampliconList = []
-ampliconList.append(query)
-
-#query.checkHybridization()
-myPredict = predict()
-myPredict.writeHandleForR(ampliconList,1)
-myListOfEfficiencies = myPredict.predictGam()
-
-ampliconList[0].setEfficiency(myListOfEfficiencies[0])
-#print name
-effect = int("".join(myListOfEfficiencies))
-print str(effect)+" amplicon are synthesized from 1 template at each cycle of the PCR for the this '"+name+"' PCR reaction"
+name = ""
 
 
-call(["rm", "gamResult.data"])
-call(["rm", "primerDataForR.dat"])
+if len(sys.argv) == 1 :
+	print "Please input sequence, forward primer sequence, and reverse primer sequence\n ./testing.py target_sequence forward_primer_sequence reverse_sequence"
+
+if len(sys.argv) <= 2 and len(sys.argv) > 1:
+	seq = str(sys.argv[1]).upper()
+	print "No primers input\nITS primers by default\nforward : TCCTCCGCTTATTGATATGC\n reverse = GGAAGTAAAAGTCGTAACAAGG"
+	forward = "TCCTCCGCTTATTGATATGC"
+	reverse = "GGAAGTAAAAGTCGTAACAAGG"
+	actual_prog(seq,forward,reverse)
+
+elif len(sys.argv) == 4 :
+	seq = str(sys.argv[1]).upper()
+	forward = str(sys.argv[2]).upper()
+	reverse = str(sys.argv[3]).upper()
+	actual_prog(seq,forward,reverse)
+
+#seq = "GTAACAAGGTTTCCGTAGGTGAACATGCGGAAGGATCATTGTCGAACCCTGCAAAGCAGAACGACCCGTGAACACGTAAAAACAACCGAGCGTCGAGTGGATTTTTTGATCCACTCGATGCTCTGTCGATGCCCATTTACTTGTGTTCTTTTGGACTCGGTGGATGTGTCATTGACGCAATAACAACCCCCGGCACAATGTGTGCCAAGGAAAACTAAACTTGAGAATGCTTGTTTCATGTTGCCCCGTTCGCGGTGTGCTCTGGATTGGCTTCTTTATAATTACAAACGACTCTCGGCAACGGATATCTCGGCTCACGCATCGATGAAGAACGTAGCAAAATGCGATACTTGGTGTGAATTGCAGAATCCCGTGAACCATCGAGTTTTTGAACGCAAGTTGCGCCCGAAGCCTTTTGGCCGAGGGCACGTCTGCCTGGGCGTCACGCATCGCGTCGCCCCCAACAAATCTTTGTTGGGAGCGGATATTGGTCTCCCGTGCTCATGGCGTGGTTGGCCAAAATAGGAGTCCCTTCGATGGACGCACGAACTAGTGGTGGTCGTAAAAACCCTCGTTCTTTGTTCTGCGTTAGTCGTAAGAAAATACTCTTCAAACACCCCAATGTGTTGTCTTAGGACGACGCTTCGACCGCGACCCCAGGTCAGGCGGGACTACCCGCTGAGTTTAAGCATATCAATAA"
 
 
-#print ampliconList[0].toString()
-
-#foo.cssBottom()
 
 
+	#print ampliconList[0].toString()
+
+	#foo.cssBottom()
